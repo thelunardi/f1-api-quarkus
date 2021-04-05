e366d43883c233261feb89bb3c9ba4872d384bd6
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-04-05 19:45:15 -0300

    add a controller to circuits

--- src/main/java/dev/thelunardi/controller/CircuitController.java
+++ src/main/java/dev/thelunardi/controller/CircuitController.java
@@ -1,7 +1,7 @@
 package dev.thelunardi.controller;
 
-import dev.thelunardi.exception.DriverException;
-import dev.thelunardi.service.DriverService;
+import dev.thelunardi.exception.CircuitException;
+import dev.thelunardi.service.CircuitService;
 import dev.thelunardi.service.DriverServiceImpl;
 import org.jboss.logging.Logger;
 
@@ -12,22 +12,22 @@ import javax.ws.rs.core.Response;
 import java.net.MalformedURLException;
 
 @Path("/f1")
-public class DriverController {
+public class CircuitController {
     private static final Logger LOG = Logger.getLogger(DriverServiceImpl.class);
 
     @Inject
-    DriverService driverService;
+    CircuitService circuitService;
 
     @GET
-    @Path("drivers")
-    public Response getDrivers(String id) throws Exception {
+    @Path("circuits")
+    public Response getCircuits(String id) throws Exception {
         try {
-            return Response.ok(driverService.getDrivers()).build();
-        } catch (MalformedURLException | DriverException e) {
-            LOG.errorf("Erro ao listar os pilotos", e.getMessage());
+            return Response.ok(circuitService.getCircuits()).build();
+        } catch (MalformedURLException | CircuitException e) {
+            LOG.errorf("Erro ao listar os circuitos", e.getMessage());
             return Response.status(Response.Status.BAD_REQUEST).build();
         } catch (Exception e) {
-            LOG.errorf("Erro interno ao listar os pilotos", e.getMessage());
+            LOG.errorf("Erro interno ao listar os circuitos", e.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
